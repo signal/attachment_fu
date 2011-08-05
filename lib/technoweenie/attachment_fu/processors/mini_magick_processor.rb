a438c785aed8f3dd834caf2df8faafd359573774
Tim Oakes <toakes@signalhq.com>
2011-08-05 14:36:53 -0500

    GIF hack fails with mini_magick 3.3

--- lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
+++ lib/technoweenie/attachment_fu/processors/mini_magick_processor.rb
@@ -41,11 +41,6 @@ module Technoweenie # :nodoc:
           img.combine_options do |commands|
             commands.strip unless attachment_options[:keep_profile]
 
-            # gif are not handled correct, this is a hack, but it seems to work.
-            if img.output =~ / GIF /
-              img.format("png")
-            end           
-            
             if size.is_a?(Fixnum) || (size.is_a?(Array) && size.first.is_a?(Fixnum))
               if size.is_a?(Fixnum)
                 size = [size, size]
